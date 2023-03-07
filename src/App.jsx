444c098865fe2e0d63fefb0d159b174bb294e7d9
us0610 <ujjwal158b@gmail.com>
2023-03-07 09:39:51 +0530

    functionality to remove budget and expenses

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,8 @@ function App() {
     const [expenseName, setExpenseName] = useState('')
     const [expenseAmount, setExpenseAmount] = useState(0)
 
+    const [selectedBudget, setSelectedBudget] = useState('')
+
     const onBudgetNameChangeHandler = (event) => {
         setBudgetName(event.target.value)
     }
@@ -47,40 +49,67 @@ function App() {
     function onExpenseSubmit(event) {
         event.preventDefault()
 
-        const budgetId = event.target.id
-        console.log(budgetId)
+        const budgetId = selectedBudget ? selectedBudget : budgets[0].bId
         budgetCtx.addExpense({ eAmount: expenseAmount, eName: expenseName, eId: uuidv4(), bId: budgetId })
+
+        setExpenseName('')
+        setExpenseAmount(0)
+    }
+
+    function onRemoveBudgetHandler(event) {
+        budgetCtx.removeBudget(event.target.id)
+    }
+
+    function removeExpense(bId, eId) {
+        budgetCtx.removeExpense(bId, eId)
+    }
+
+    function clearLocalStorage() {
+        localStorage.clear()
     }
 
 
     return (
         <main>
+            <button onClick={clearLocalStorage}>clear local storage</button>
             <form onSubmit={onBudgetSubmit}>
                 <input type="text" name="budget name" onChange={onBudgetNameChangeHandler} value={budgetName} placeholder="enter budget name" />
                 <input onChange={onBudgetAmountChangeHandler} value={budgetAmount} type="number" name="budget amount" placeholder="enter budget amount" />
                 <button type="submit">add budget</button>
             </form>
-            <form onSubmit={onExpenseSubmit} id={budgets.length > 0 ? budgets[0].bId : ''}>
-                <input type="text" name="expense name" onChange={onExpenseNameChangeHandler} value={expenseName} placeholder="enter expense name" />
-                <input onChange={onExpenseAmountChangeHandler} value={expenseAmount} type="number" name="expense amount" placeholder="enter expense amount" />
-                <button type="submit">add expense</button>
-            </form>
+            {budgets.length > 0 && (
+                <form onSubmit={onExpenseSubmit}>
+                    <input type="text" name="expense name" onChange={onExpenseNameChangeHandler} value={expenseName} placeholder="enter expense name" />
+                    <input onChange={onExpenseAmountChangeHandler} value={expenseAmount} type="number" name="expense amount" placeholder="enter expense amount" />
+                    {budgets.length > 1 &&
+                        <select value={selectedBudget} onChange={e => setSelectedBudget(e.target.value)}>
+                            {
+                                budgets.map(budget => {
+                                    return <option key={budget.bId} value={budget.bId}>{budget.bName}</option>
+                                })
+                            }
+                        </select>
+                    }
+                    <button type="submit">add expense</button>
+                </form>
+            )}
             <div>
                 {
                     budgets.map(budget => {
                         return (
                             <div key={budget.bId}>
-                                <p>bName: {budget.bName}</p>
-                                <p>bAmount: {budget.bAmount}</p>
-                                <p>bId: {budget.bId}</p>
-                                {budget.expenses.map((expense) => (
-                                    <div key={expense.eId} style={{margin: '10px', background: 'black', padding: '10px'}}>
-                                        <p>bId: {expense.bId}</p>
-                                        <p>eId: {expense.eId}</p>
-                                        <p>eName: {expense.eName}</p>
-                                        <p>eAmount: {expense.eAmount}</p>
-                                    </div>
-                                ))}
+                                <p>budget name: {budget.bName}</p>
+                                <p>budget amount: {budget.bAmount}</p>
+                                {budgets && (
+                                    budget.expenses.map((expense) => (
+                                        <div key={expense.eId} style={{ margin: '10px', background: 'black', padding: '10px' }}>
+                                            <p>expense name: {expense.eName}</p>
+                                            <p>expense amount: {expense.eAmount}</p>
+                                            <button onClick={() => { removeExpense(expense.bId, expense.eId) }} >remove expense</button>
+                                        </div>
+                                    ))
+                                )}
+                                <button onClick={onRemoveBudgetHandler} id={budget.bId}>remove {budget.bName}</button>
                             </div>
                         )
                     })
--- src/store/budget-context.jsx
+++ src/store/budget-context.jsx
@@ -3,43 +3,75 @@ import { createContext, useReducer, useEffect } from "react";
 const initialBudgetState = []
 
 const budgetReducer = (state, action) => {
-    switch (action.type) {
-        case "ADD_BUDGET":
-            const budgets = [...state, {
-                bName: action.payload.bName,
-                bAmount: action.payload.bAmount,
-                bId: action.payload.bId,
-                expenses: []
-            }]
+    if (action.type === "ADD_BUDGET") {
+        const budgets = [...state, {
+            bName: action.payload.bName,
+            bAmount: action.payload.bAmount,
+            bId: action.payload.bId,
+            expenses: []
+        }]
 
-            localStorage.setItem('budgets', JSON.stringify(budgets))
+        localStorage.setItem('budgets', JSON.stringify(budgets))
 
-            return budgets
-
-        case "ADD_EXPENSE":
-            const oldBudgetIndex = state.findIndex((budget) => {
-                return budget.bId === action.payload.bId
-            })
-
-            const newExpenses = [{
-                bId: action.payload.bId,
-                eName: action.payload.eName,
-                eAmount: action.payload.eAmount,
-                eId: action.payload.eId
-            }, ...state[oldBudgetIndex].expenses]
-
-            const newBudget = {
-                ...state[oldBudgetIndex],
-                expenses: newExpenses
-            }
-
-            state[oldBudgetIndex] = newBudget
-
-            return [...state]
-
-        default:
-            return initialBudgetState
+        return budgets
     }
+
+    if (action.type === "ADD_EXPENSE") {
+        const oldBudgetIndex = state.findIndex((budget) => {
+            return budget.bId === action.payload.bId
+        })
+
+        const newExpenses = [{
+            bId: action.payload.bId,
+            eName: action.payload.eName,
+            eAmount: action.payload.eAmount,
+            eId: action.payload.eId
+        }, ...state[oldBudgetIndex].expenses]
+
+        const newBudget = {
+            ...state[oldBudgetIndex],
+            expenses: newExpenses
+        }
+
+        state[oldBudgetIndex] = newBudget
+
+        return [...state]
+    }
+
+    if (action.type === "REMOVE_BUDGET") {
+        const budgetId = action.payload
+
+        const newBudgets = state.filter((budget) => {
+            return budget.bId !== budgetId
+        })
+
+        return newBudgets
+    }
+
+    if (action.type === "REMOVE_EXPENSE") {
+        const budgetIndex = state.findIndex((budget) => {
+            console.log(budget, action.payload.bId)
+            return budget.bId === action.payload.bId
+        })
+
+        console.log(budgetIndex)
+
+        const budget = state[budgetIndex]
+
+        console.log(budget)
+
+        const newBudget = {
+            ...budget, expenses: budget.expenses.filter(exp => {
+                return exp.eId !== action.payload.eId
+            })
+        }
+
+        state[budgetIndex] = newBudget
+
+        return [...state] 
+    }
+
+    return initialBudgetState
 }
 
 export const budgetContext = createContext(
@@ -47,7 +79,8 @@ export const budgetContext = createContext(
         budgets: [],
         addBudget: () => { },
         addExpense: () => { },
-        removeBudget: () => { }
+        removeBudget: () => { },
+        removeExpense: () => { }
     }
 )
 
@@ -63,15 +96,24 @@ const BudgetContextProvider = ({ children }) => {
         dispatchBudget({ type: "ADD_BUDGET", payload: budget })
     }
 
+    const removeBudget = (budgetId) => {
+        dispatchBudget({ type: "REMOVE_BUDGET", payload: budgetId })
+    }
+
     const addExpense = (bId) => {
         dispatchBudget({ type: "ADD_EXPENSE", payload: bId })
+    }
+
+    const removeExpense = (bId, eId) => {
+        dispatchBudget({type: "REMOVE_EXPENSE", payload: {bId, eId}})
     }
 
     const budgetContextValue = {
         budgets: budgetsState,
         addBudget: addBudget,
-        removeBudget: () => { },
-        addExpense: addExpense
+        removeBudget: removeBudget,
+        addExpense: addExpense,
+        removeExpense: removeExpense
     }
 
     return <budgetContext.Provider value={budgetContextValue}>{children}</budgetContext.Provider>

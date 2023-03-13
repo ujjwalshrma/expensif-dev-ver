fc256cde4c39ae23ee818d970bb948be4407f63b
us0610 <ujjwal158b@gmail.com>
2023-03-13 08:50:30 +0530

    added budget accordian feature

--- src/components/Budgets/BudgetForm.jsx
+++ src/components/Budgets/BudgetForm.jsx
@@ -4,10 +4,14 @@ import { v4 as uuidv4 } from 'uuid';
 
 import Card from "../UI/Card/Card";
 
+import plus from '../../../public/plus.svg'
+
 const BudgetForm = ({ addBudget }) => {
 
     const [budgetName, setBudgetName] = useState('')
     const [budgetAmount, setBudgetAmount] = useState('')
+
+    const [accordian, setAccordian] = useState(false)
 
     const onBudgetNameChangeHandler = (event) => {
         setBudgetName(event.target.value)
@@ -30,15 +34,29 @@ const BudgetForm = ({ addBudget }) => {
         return
     }
 
+    const onAccordianHandler = () => {
+        setAccordian(!accordian)
+    }
+
+
+    const plugIconClass = `${styles.plus} ${accordian ? styles.open : ''}`
+    const accordianClass = `${styles.accordian} ${accordian ? styles.open : ''}`
+
     return (
-        <Card color='g' className={styles.budget__card}>
-            <h1 className={styles.budget__form__heading}>Create Budget</h1>
-            <form onSubmit={onBudgetSubmit} className={styles.budget__form}>
-                <input type="text" name="budget name" onChange={onBudgetNameChangeHandler} value={budgetName} placeholder="Budget Name" />
-                <input onChange={onBudgetAmountChangeHandler} min='0' value={budgetAmount} type="number" name="budget amount" placeholder="enter budget amount" />
-                <button type="submit">Add Budget</button>
-            </form>
-        </Card>
+        <div className={accordianClass}>
+            <div onClick={onAccordianHandler} className={styles.accordian__header}>
+                <h1>Create Budget</h1>
+                <img className={plugIconClass} src={plus} alt="Plus Icon" />
+            </div>
+            <Card color='g' className={styles.budget__card}>
+                <h1 className={styles.budget__form__heading}>Create Budget</h1>
+                <form onSubmit={onBudgetSubmit} className={styles.budget__form}>
+                    <input type="text" name="budget name" onChange={onBudgetNameChangeHandler} value={budgetName} placeholder="Budget Name" />
+                    <input onChange={onBudgetAmountChangeHandler} min='0' value={budgetAmount} type="number" name="budget amount" placeholder="enter budget amount" />
+                    <button type="submit">Add Budget</button>
+                </form>
+            </Card>
+        </div>
     )
 }
 
